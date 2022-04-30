762307d0f7362fd24b4189078593d947983eb209
KIM MIN BO <68101836+minbo2002@users.noreply.github.com>
2022-04-30 18:35:32 +0900

    TodoServiceTest 구현

--- src/test/java/com/example/todotest/service/TodoServiceTest.java
+++ src/test/java/com/example/todotest/service/TodoServiceTest.java
@@ -14,12 +14,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @Slf4j
@@ -40,7 +44,7 @@ class TodoServiceTest {
                 .id(1L)
                 .title("test_title")
                 .order(1L)
-                .complted(false)
+                .completed(false)
                 .build();
     }
 
@@ -52,7 +56,7 @@ class TodoServiceTest {
         TodoRequest todoRequest = TodoRequest.builder()
                 .title(todo.getTitle())
                 .order(todo.getOrder())
-                .completed(todo.getComplted())
+                .completed(todo.getCompleted())
                 .build();
 
         TodoResponse todoResponse = todoService.add(todoRequest);
@@ -64,6 +68,71 @@ class TodoServiceTest {
     }
 
     @Test
-    void searchById() {
+    public void searchById() {
+
+        given(this.todoRepository.findById(1L))
+                .willReturn(Optional.of(todo));
+
+        TodoResponse response = this.todoService.searchById(1L);
+
+        log.info("response: {}", response);
+
+        assertThat(response.getTitle()).isEqualTo("test_title");
+        assertThat(response.getOrder()).isEqualTo(1L);
+    }
+
+    @Test
+    void searchAll() {
+        // given - precondition or setup
+        Todo todo1 = Todo.builder()
+                .id(100L)
+                .title("test_dsg")
+                .order(1L)
+                .completed(true)
+                .build();
+
+        given(todoRepository.findAll()).willReturn(List.of(todo, todo1));
+
+        // when - action or the behaviour that we are going test
+        List<TodoResponse> todoResponses = todoService.searchAll();
+        log.info("todoResponses: {}", todoResponses);
+
+        // then - verify the output
+        assertThat(todoResponses).isNotNull();
+        assertThat(todoResponses.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void updateById(){
+        // given - precondition or setup
+        given(todoRepository.findById(anyLong()))
+                .willReturn(Optional.of(todo));
+        given(todoRepository.save(todo)).willReturn(todo);      // updateById면 두 상황 모두 만들어져야돼!
+
+        TodoRequest updateRequest = TodoRequest.builder()
+                .title("kkk")
+                .order(2L)
+                .build();
+
+        // when - action or the behaviour that we are going test
+        TodoResponse todoResponse = todoService.updateById(todo.getId(), updateRequest);
+        log.info("todoResponses: {}", todoResponse);
+        // then - verify the output
+        assertThat(todoResponse.getTitle()).isEqualTo(updateRequest.getTitle());
+        assertThat(todoResponse.getOrder()).isEqualTo(updateRequest.getOrder());
+
+    }
+
+    @Test
+    public void deleteById(){
+        // given - precondition or setup
+        Long todoId = 1L;
+        willDoNothing().given(todoRepository).deleteById(todoId);
+
+        // when - action or the behaviour that we are going test
+        todoService.deleteById(todoId);
+        // then - verify the output
+        verify(todoRepository).deleteById(todoId);
+
     }
 }
